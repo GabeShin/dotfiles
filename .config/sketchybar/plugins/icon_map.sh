69de2e41b35f841c87861d1c0400b044913b918c
Gabe <gabeshin.ts@gmail.com>
2025-04-18 20:05:18 +0900

    icons issue

--- .config/sketchybar/plugins/icon_map.sh
+++ .config/sketchybar/plugins/icon_map.sh
@@ -235,7 +235,7 @@ function __icon_map() {
         icon_result=":iris:"
         ;;
    "iTerm" | "iTerm2")
-        icon_result=":terminal:"
+        icon_result=":iterm:"
         ;;
    "Jellyfin Media Player")
         icon_result=":jellyfin:"
