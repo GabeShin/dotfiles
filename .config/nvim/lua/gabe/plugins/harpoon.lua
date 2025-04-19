5cb3a2bbf3da897d8cb9014e2993fae09f35c1fc
Gabe <gabeshin.ts@gmail.com>
2025-04-19 10:20:08 +0900

    add harpoon

--- .config/nvim/lua/gabe/plugins/harpoon.lua
+++ .config/nvim/lua/gabe/plugins/harpoon.lua
@@ -1,48 +1,60 @@
 return {
-	"ThePrimeagen/harpoon",
+	"thePrimeagen/harpoon",
 	branch = "harpoon2",
-	requires = { { "nvim-lua/plenary.nvim" } },
+	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local harpoon = require("harpoon")
-		local keymap = vim.keymap
-
-		keymap.set("n", "<leader>a", function()
-			harpoon.mark.add_file()
-		end, { desc = "Add file to harpoon" })
-
-		keymap.set("n", "<leader>1", function()
-			harpoon.ui.nav_file(1)
-		end, { desc = "Navigate to file 1" })
-		keymap.set("n", "<leader>2", function()
-			harpoon.ui.nav_file(2)
-		end, { desc = "Navigate to file 2" })
-		keymap.set("n", "<leader>3", function()
-			harpoon.ui.nav_file(3)
-		end, { desc = "Navigate to file 3" })
-		keymap.set("n", "<leader>4", function()
-			harpoon.ui.nav_file(4)
-		end, { desc = "Navigate to file 4" })
-		keymap.set("n", "<leader>5", function()
-			harpoon.ui.nav_file(5)
-		end, { desc = "Navigate to file 5" })
-		keymap.set("n", "<leader>6", function()
-			harpoon.ui.nav_file(6)
-		end, { desc = "Navigate to file 6" })
-		keymap.set("n", "<leader>7", function()
-			harpoon.ui.nav_file(7)
-		end, { desc = "Navigate to file 7" })
-		keymap.set("n", "<leader>8", function()
-			harpoon.ui.nav_file(8)
-		end, { desc = "Navigate to file 8" })
-		keymap.set("n", "<leader>9", function()
-			harpoon.ui.nav_file(9)
-		end, { desc = "Navigate to file 9" })
-
-		vim.keymap.set("n", "", function()
-			harpoon:list():prev()
-		end)
-		vim.keymap.set("n", "<leader>l", function()
-			harpoon:list():next()
-		end)
+		require("harpoon"):setup()
 	end,
+	keys = {
+		{
+			"<leader>a",
+			function()
+				require("harpoon"):list():append()
+			end,
+			desc = "harpoon file",
+		},
+		{
+			"<leader>fh",
+			function()
+				local harpoon = require("harpoon")
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end,
+			desc = "harpoon quick menu",
+		},
+		{
+			"<leader>1",
+			function()
+				require("harpoon"):list():select(1)
+			end,
+			desc = "harpoon to file 1",
+		},
+		{
+			"<leader>2",
+			function()
+				require("harpoon"):list():select(2)
+			end,
+			desc = "harpoon to file 2",
+		},
+		{
+			"<leader>3",
+			function()
+				require("harpoon"):list():select(3)
+			end,
+			desc = "harpoon to file 3",
+		},
+		{
+			"<leader>4",
+			function()
+				require("harpoon"):list():select(4)
+			end,
+			desc = "harpoon to file 4",
+		},
+		{
+			"<leader>5",
+			function()
+				require("harpoon"):list():select(5)
+			end,
+			desc = "harpoon to file 5",
+		},
+	},
 }
