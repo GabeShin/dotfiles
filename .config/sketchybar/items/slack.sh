a5936a375aad63c9a036546e2061267f66cbca98
Taesub <taesub@enhans.ai>
2025-05-12 10:04:43 +0900

    fix slack icon

--- .config/sketchybar/items/slack.sh
+++ .config/sketchybar/items/slack.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 sketchybar --add item slack right
-sketchybar --set slack icon="" update_freq=60 script="${CONFIG_DIR}/plugins/slack.sh"
+sketchybar --set slack \
+   icon=":slack:" \
+   icon.font='sketchybar-app-font:Regular:16.0'
+   update_freq=60 \
+   script="${CONFIG_DIR}/plugins/slack.sh"
 
